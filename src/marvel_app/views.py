b093e027827158752d2a00cae9cc0ee94d9dfe6d
tomas9807 <tomas9807@protonmail.com>
2018-12-09 01:38:55 -0400

    working dynamic lists

--- src/marvel_app/views.py
+++ src/marvel_app/views.py
@@ -3,6 +3,15 @@ from flask import render_template,redirect,flash,request,jsonify,json
 from .forms import NewPersonaje
 from . import models
 
+tipos_geograficos = ('pais','estado','ciudad','zona')
+def get_list_lugares(tipo_lugar,id_padre=None):
+    return models.select_query(f"""
+                    select lugar.id,lugar.nombre from Lugar as lugar
+                    where lugar.{'tipo_geografico' if tipo_lugar in tipos_geograficos else 'tipo'}='{tipo_lugar}'
+                    {f'and lugar.id_padre={id_padre}' if id_padre else ''}
+                    """ 
+                    )
+
 @app.route('/')
 def home():
     title = 'home'
@@ -38,7 +47,29 @@ def personajes(keyword=None,data_type=None,pk=None):
                 colores = models.select_query('select id,nombre from Color')
                 form.color_ojos.choices = colores
                 form.color_pelo.choices = colores
-                form.lugar_nacimiento_universo.choices = models.select_query('select id,nombre from Lugar where tipo="universo" ')
+                universos = get_list_lugares('universo')
+                form.lugar_nacimiento_universo.choices = universos
+                if universos:
+                    planetas = get_list_lugares('planeta',universos[0][0])
+                    if planetas:
+                        form.lugar_nacimiento_planeta.choices = planetas
+                        paises = get_list_lugares('pais',planetas[0][0])
+                        if paises:
+                            form.lugar_nacimiento_pais.choices = paises
+                            estados = get_list_lugares('estado',paises[0][0])
+                            if estados:
+                                form.lugar_nacimiento_estado.choices = estados
+                                ciudades = get_list_lugares('ciudad',estados[0][0])
+                                if ciudades:
+                                    form.lugar_nacimiento_ciudad.choices = ciudades
+                                    zonas = get_list_lugares('zona',ciudades[0][0])
+                                    if zonas:
+                                        form.lugar_nacimiento_zona_especifica.choices = zonas
+
+
+
+                    
+               
                 return render_template('new.html',title=title,form=form,type='personaje')
 
     
@@ -57,20 +88,11 @@ def personajes(keyword=None,data_type=None,pk=None):
 
     return render_template('personajes.html',title=title)
 
-def get_tipo_str(tipo):
-    tipos_geograficos = ('pais','estado','ciudad','zona')
-    return 'tipo_greografico' if tipo in tipos_geograficos else 'tipo'
-
-@app.route('/lugares/get/<tipo_padre>/<id_padre>/<tipo_hijo>',methods=['POST','GET'])
-@app.route('/lugares/get/<tipo_padre>/<id_padre>/<tipo_hijo>/<pk>',methods=['POST','GET'])
-def lugares(tipo_padre,id_padre,tipo_hijo,pk=None):
-    tipo_str_hijo = get_tipo_str(tipo_hijo)
-    if not pk:
-        data = models.select_query(f"""
-        select hijo.id,hijo.nombre from Lugar as hijo where hijo.tipo={tipo_str_hijo} and hijo.id_padre={id_padre}
-        """ 
-        )
-        return jsonify(data)
+    
+@app.route('/lugares/get/<tipo_hijo>/<id_padre>',methods=['POST','GET'])
+def lugares(tipo_hijo,id_padre):
+    data = get_list_lugares(tipo_hijo,id_padre)
+    return jsonify(data)
 
 
 
